1e27ad053d0e07902bfe86be640d2c932b526105
sub-t <subt@outlook.jp>
2023-02-14 17:11:28 +0900

    💄 headingのlineHeightの指定
    共通テーマ(styles/theme)のheading(h1～h4)のlineHeightを指定。

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,24 +1,27 @@
 import { createTheme } from '@mui/material/styles';
 
-// Create a theme instance.
 export const theme = createTheme({
   typography: {
     fontFamily: 'system-ui',
     h1: {
-      fontWeight: 700,
+      lineHeight: '1rem',
       fontSize: '3rem',
+      fontWeight: 700,
     },
     h2: {
-      fontWeight: 600,
+      lineHeight: '2.25rem',
       fontSize: '1.875rem',
+      fontWeight: 600,
     },
     h3: {
-      fontWeight: 600,
+      lineHeight: '2rem',
       fontSize: '1.5rem',
+      fontWeight: 600,
     },
     h4: {
-      fontWeight: 600,
+      lineHeight: '1.75rem',
       fontSize: '1.25rem',
+      fontWeight: 600,
     },
   },
   spacing: (factor: number) => `${0.25 * factor}rem`,
